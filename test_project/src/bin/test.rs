3bb88d862618540789a4f66ae9973e684374ecaf
Amit-Weis <amitweisgor@gmail.com>
2025-03-10 15:38:32 -0400

    added keyboard input fast and good :)

--- test_project/src/bin/test.rs
+++ test_project/src/bin/test.rs
@@ -1,15 +1,29 @@
 extern crate keyboard_query;
+use std::thread;
+use std::sync::mpsc;
+use std::time;
 
 use keyboard_query::{DeviceQuery, DeviceState};
 
 fn main() {
-    let device_state = DeviceState::new();
-    let mut prev_keys = vec![];
-    loop {
-        let keys = device_state.get_keys();
-        if keys != prev_keys {
-            println!("{:?}", keys);
+    let device_state = DeviceState::new(); 
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let device_state = DeviceState::new(); 
+        loop {
+            let keys = device_state.get_keys();
+            if tx.send(keys).is_err() {
+                break; // Stop if receiver is closed
+            }
+            thread::sleep(time::Duration::from_millis(1)); // Prevent CPU overuse
         }
-        prev_keys = keys;
+    });
+
+    loop {
+        match rx.recv() {
+            Ok(keys) => println!("{:?}", keys),
+            Err(_) => break, // Exit loop if sender stops
+        }
     }
 }
